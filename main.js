1f0005d9999429ddc32ae065c2e75f92e89282fe
Kuldeep Singh <68851011+kuldeep998@users.noreply.github.com>
2024-05-16 23:31:57 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req, res) {
-    res.write('Welcome to NODE JS Application ');
-    res.end();
-}).listen(8000);
-
-console.log('Application loaded successfully');
