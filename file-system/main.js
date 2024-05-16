14da5205b2cba1e72dc8010d58e3fc0bd965371a
kuldeep.argal <kuldeep.argal@gtcsys.com>
2024-05-16 23:55:09 +0530

    File module file added successfully

---
+++ file-system/main.js
@@ -0,0 +1,46 @@
+var http = require('http');
+var fs = require('fs');
+
+// Example 1
+fs.appendFile('file2.txt', 'Hi I am kuldeep singh', function(err) {
+    if (err) {
+        throw err;
+    }
+    console.log('File created successfully');
+});
+
+// Example 2
+fs.appendFile('file2.txt', 'Hi Now data will append', function(err) {
+    if (err) {
+        throw err;
+    }
+    console.log('In file content appended');
+});
+
+// Example 3
+fs.open('myfile2.txt', 'w', function(err, file) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('Saved!');
+})
+
+// Example 4
+fs.writeFile('file2.txt', 'File data override', function(err) {
+    if (err) {
+        throw err;
+    }
+    console.log('File data override successfully');
+});
+
+http.createServer(function(req, res) {
+    // read file
+    fs.readFile('file.html', function(err, data) {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.write(data);
+        return res.end();
+    });
+}).listen(8000);
+
+console.log('Application started successfully');
